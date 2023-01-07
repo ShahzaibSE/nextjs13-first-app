fce08b96c2b9739cdc14ba7356839c04850b841e
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2023-01-08 01:20:59 +0500

    [FEAT] - Implemented Chark UI in next 13 app

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
+'use client';
 
-export default function page() {
-  return (
-    <div>Your Gallery</div>
-  )
+import React from 'react'
+import { useRouter } from 'next/navigation'
+import {Button} from "@chakra-ui/react";
+import { ChakraProvider } from '@chakra-ui/react'
+
+
+export default function Gallery() {
+    const router = useRouter();
+    return (
+        <ChakraProvider>
+            <div>
+                <div>Your Gallery</div>
+                <br/>
+                <div>
+                    <Button colorScheme="blue" onClick={()=>router.push("/gallery/animals")}>Animals</Button>
+                </div>
+            </div>
+        </ChakraProvider>
+    )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from './page.module.css'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -8,6 +9,11 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div>
+        <div>
+          <Link href="/home">Home</Link>
+          <Link href="/gallery">Gallery</Link>
+        </div>
+        <br/>
         <div className={styles.greeting_text_container}>
           <h1>Hello World from Shahzaib Noor</h1>
         </div>
