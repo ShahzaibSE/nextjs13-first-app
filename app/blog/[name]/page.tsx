91ecde3e8cdbdba2c1b0906422b1c90e85478854
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2023-01-09 02:08:37 +0500

    [FEAT] - Generating dynamic segments on runtime using generaticStaticParams function

--- app/blog/[name]/page.tsx
+++ app/blog/[name]/page.tsx
@@ -1,7 +1,18 @@
-export default function GiveName({
-  params,
-}: {
-  params: { name: string };
-}) {
+export async function generateStaticParams() {
+  const names: string[] = [
+    "zia",
+    "shahzaib",
+    "zeeshan",
+    "hira",
+    "adil",
+    "rehan",
+  ];
+
+  return names.map((name) => ({
+    name: name,
+  }));
+}
+
+export default function GiveName({ params }: { params: { name: string } }) {
   return <div>My name is {params.name}.</div>;
 }
